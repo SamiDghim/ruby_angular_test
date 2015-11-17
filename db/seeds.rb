d0e2dbba60d6b82795a757bf450175331a065b36
SamiDghim <samidghim@gmail.com>
2015-11-17 20:56:42 +0200

    initialize the ingredient table using the seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 
-#  Create an user
+#  Create a user
 puts 'Create a user '
 User.first_or_create( email: 'admin@example.com',password:'password',name:'admin')
 User.first_or_create(email: 'user1@example.com',password:'password',name:'user1')
@@ -7,12 +7,11 @@ User.first_or_create(email: 'user2@example.com',password:'password',name:'user2'
 User.first_or_create(email: 'user3@example.com',password:'password',name:'user3')
 User.first_or_create(email: 'user4@example.com',password:'password',name:'user5')
 # create a list of ingredients
-puts 'Create a a list of ingredients '
-Ingredient.create(name:'ingrédient1')
-Ingredient.create(name:'ingrédient2')
-Ingredient.create(name:'ingrédient3')
-Ingredient.create(name:'ingrédient4')
-Ingredient.create(name:'ingrédient5')
-Ingredient.create(name:'ingrédient6')
-Ingredient.create(name:'ingrédient7')
+puts 'Create a list of ingredients '
+Ingredient.create(name:'sugar')
+Ingredient.create(name:'salt')
+Ingredient.create(name:'eggs')
+Ingredient.create(name:'peppers')
+Ingredient.create(name:'olive oil')
+ 
 
