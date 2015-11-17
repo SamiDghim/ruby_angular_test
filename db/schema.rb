087fddf65c72909faab7e660e633d20e90824e58
SamiDghim <samidghim@gmail.com>
2015-11-17 20:49:56 +0200

    generate new model'Ingredient',add new attribute(name) to model'User'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116154936) do
+ActiveRecord::Schema.define(version: 20151117183810) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +32,7 @@ ActiveRecord::Schema.define(version: 20151116154936) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,18 @@
 
 #  Create an user
-
 puts 'Create a user '
-User.first_or_create( email: 'admin@example.com', password: 'password' )
+User.first_or_create( email: 'admin@example.com',password:'password',name:'admin')
+User.first_or_create(email: 'user1@example.com',password:'password',name:'user1')
+User.first_or_create(email: 'user2@example.com',password:'password',name:'user2')
+User.first_or_create(email: 'user3@example.com',password:'password',name:'user3')
+User.first_or_create(email: 'user4@example.com',password:'password',name:'user5')
+# create a list of ingredients
+puts 'Create a a list of ingredients '
+Ingredient.create(name:'ingrédient1')
+Ingredient.create(name:'ingrédient2')
+Ingredient.create(name:'ingrédient3')
+Ingredient.create(name:'ingrédient4')
+Ingredient.create(name:'ingrédient5')
+Ingredient.create(name:'ingrédient6')
+Ingredient.create(name:'ingrédient7')
+
