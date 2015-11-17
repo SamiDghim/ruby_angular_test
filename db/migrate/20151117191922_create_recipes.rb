7fe02632b6524aaf994d456da1a78a9bb6dc2285
SamiDghim <samidghim@gmail.com>
2015-11-17 21:19:54 +0200

    generate the 'recipe' model and create his table

---
+++ db/migrate/20151117191922_create_recipes.rb
@@ -0,0 +1,14 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.text :description
+      t.integer :rating
+      t.boolean :isfavorite
+      t.string :photo
+      t.string :user
+
+      t.timestamps null: false
+    end
+  end
+end
