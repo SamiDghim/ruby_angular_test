80d982e3a7010d0205ea2ba5bd5d83bbc147affe
SamiDghim <samidghim@gmail.com>
2015-11-18 19:20:43 +0200

    add some style to recipes list and sort it by date of publication

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,28 @@
 class RecipesController < ApplicationController
-
+  protect_from_forgery with: :exception
+  protect_from_forgery except: :sign_in
 respond_to :json
 
   def index
-    respond_with Recipe.all
+   #@recipes = #.paginate(page: params[:page], per_page: 2)
+
+   # return all recipes sorted by date of publication.
+   respond_with Recipe.order(created_at: :desc).all
   end
+
+
+ 
+ # def index
+   # respond_to do |format|
+     # format.json do
+      #  page = params[:page].present? ? params[:page] : 1
+     #   @recipes = Recipe.all.paginate(:page => page, :per_page => 5)
+    #    render :json => Paginator.pagination_attributes(@recipes).merge!(:recipes => @recipes)
+   #   end
+  #    format.html
+ #   end
+ # end
+
   def show
     respond_with Recipe.find(params[:id])
   end
